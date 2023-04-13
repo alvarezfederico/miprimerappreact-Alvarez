153d105b7a8278b866a9fa2e107e1032cf643ee5
alvarezfederico <alvarez.federico.ignacio@gmail.com>
2023-04-12 22:23:24 -0300

    proyecto react

---
+++ src/components/CardWidget/CardWidget.js
@@ -0,0 +1,13 @@
+import cart from './assets/3377039_logo_media_social_spotify_icon.svg'
+
+const CardWidget = () => {
+    return (
+        <div>
+            <img src={cart} alt="cart-widget"/>
+            0
+        </div>
+   )
+}
+
+
+export default CardWidget
---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,18 @@
+import CardWidget from "../CardWidget/CardWidget"
+
+
+const NavBar = () => {
+	return (
+		<nav>
+		<h3> Digital Commerce </h3>
+		<div>
+			<button>Celulares</button>
+			<button>Computadoras</button>
+			<button>Tablets</button>
+		</div>
+		<CardWidget/>
+		</nav>
+)
+}
+
+export default NavBar 
